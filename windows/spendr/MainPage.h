9fa118b88c39813d0892f826ab249cc2a5ef0fa0
lucasvt22 <lucas22@vt.edu>
2024-07-14 21:40:09 -0400

    Initial Commit

---
+++ windows/spendr/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::spendr::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::spendr::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
