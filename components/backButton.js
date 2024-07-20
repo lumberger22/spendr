80889f804e4fbdc778152c4cab1ffe1c8038818f
lucasvt22 <lucas22@vt.edu>
2024-07-20 19:16:04 -0400

    Create back button component

---
+++ components/backButton.js
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { ChevronLeftIcon } from 'react-native-heroicons/outline'
+import { useNavigation } from '@react-navigation/native'
+import { colors } from '../theme';
+
+export default function BackButton() {
+
+    const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity onPress={() => navigation.goBack()} className="bg-white rounded-full h-8 w-8 border-gray-200">
+        <ChevronLeftIcon size="35" color={colors.button} />
+    </TouchableOpacity> 
+  )
+}
