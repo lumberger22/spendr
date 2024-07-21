323e7c01197bf45699e4054f6c5993cbb1a184da
lucasvt22 <lucas22@vt.edu>
2024-07-21 16:54:29 -0400

    Add expense categories colors

--- theme/index.js
+++ theme/index.js
@@ -1,4 +1,12 @@
 export const colors = {
     heading: "text-black",
-    button: '#50C878',
+    button: '#50C878', // green
+}
+
+export const expenseColors = {
+    food: '#FFF9D0', // yellow
+    shopping: '#FFB19F',  // red
+    transport: '#D0F0FF',    // blue
+    entertainment: '#FFD6C2', // orange
+    other: '#E0FFE0',  // green
 }
