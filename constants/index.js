cd043447c094f5612e1eae7f147a3209041d041a
lucasvt22 <lucas22@vt.edu>
2024-07-21 16:53:30 -0400

    Add expense category colors

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const categories = [
+    {
+        title: 'Food',
+        value: 'food',
+    },
+    {
+        title: 'Shopping',
+        value: 'shopping',
+    },
+    {
+        title: 'Transport',
+        value: 'transport',
+    },
+    {
+        title: 'Entertainment',
+        value: 'entertainment',
+    },
+    {
+        title: 'Other',
+        value: 'other',
+    },
+]
