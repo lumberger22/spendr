abcd6a3ed4c98fc08e24ab2ac433a58879f109f0
lucasvt22 <lucas22@vt.edu>
2024-07-22 14:55:57 -0400

    Add onpress functionality to location cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -73,7 +73,7 @@ export default function HomeScreen() {
                         className="mx-1"
                         renderItem={({item}) => {
                             return (
-                                <TouchableOpacity className="bg-white p-3 rounded-2xl mb-3 shadow-sm">
+                                <TouchableOpacity onPress={() => navigation.navigate('TripExpenses', {...item})} className="bg-white p-3 rounded-2xl mb-3 shadow-sm">
                                     <View>
                                         <Image source={randomImage()} className="w-36 h-36 mb-2" />
                                         <Text className={`${colors.heading} font-bold`}>{item.place}</Text>
