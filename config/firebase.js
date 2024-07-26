c9bf20ff44b0b398f48e3b206ba86bb40cfd30ca
lucasvt22 <lucas22@vt.edu>
2024-07-26 15:05:42 -0400

    Add AsyncStorage for auth

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore, collection } from "firebase/firestore";
+import { getFirestore, collection } from 'firebase/firestore';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +19,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+// Initialize Firebase Auth with React Native AsyncStorage
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 
 
 export const tripsRef = collection(db, "trips");
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import user from './slices/user'
-
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import user from './slices/user';
 const store = configureStore({
   reducer: {
     user
   },
-})
+  middleware: (getDefaultMiddleware)=>
+    getDefaultMiddleware({
+        serializableCheck: false
+    })
+});
 
-export default store
+export default store;
